a8c4d9c5ab9e0bc1ec6cfa5f50bb4f279d7b11d4
Jadyn <jadynseelig@gmail.com>
2023-03-31 15:59:47 -0500

    working with feature 5

--- app.js
+++ app.js
@@ -1,1 +1,46 @@
-console.log('Hello')
+var nameInput = prompt('Hello, what is your name?')
+let input = prompt("Guess a number between 1 & 30 " + nameInput + '!')
+let number = Number(input);
+let secretNum = 19
+var counts = 1
+var oldGuesses = []
+
+if (!isNaN(input)) {
+oldGuesses.push(input);
+}
+
+
+
+
+//Feature 1 and 2 - Guessing section - Higher or lower
+while (number !== secretNum) {
+    if (Number.isNaN(number)) {
+        alert('Not a valid number')
+        number = Number(prompt('Guess Again ' + nameInput + '!'));
+        oldGuesses.push(number)
+} else if (number < secretNum) {
+        number = Number(prompt('Guess Higher ' + nameInput + '!'));
+        counts++;
+        oldGuesses.push(number);
+} else if (number > secretNum) {
+        number = Number(prompt('Guess Lower ' + nameInput + '!'));
+        counts++;
+        oldGuesses.push(number);
+    } 
+}
+
+if(number === secretNum) {
+    alert(`You got it ${nameInput}! Your number of tries was ${counts}! Your guesses were ${oldGuesses}!`)
+    } 
+
+
+
+
+
+
+
+
+
+
+
+
