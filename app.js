d07aa5b39cddcc56133857b58a9fa17e59127428
Jadyn <jadynseelig@gmail.com>
2023-04-04 12:22:02 -0500

    guessing game finished

--- app.js
+++ app.js
@@ -1,37 +1,65 @@
-var nameInput = prompt('Hello, what is your name?')
-let input = prompt("Guess a number between 1 & 30 " + nameInput + '!')
+var userName = prompt('Hello, what is your name?')
+let playAgain = true;
+let players = {}
+let secretNum = Math.floor(Math.random()* 10) + 1
+
+while (playAgain) {
+//var userName = prompt('Hello, what is your name?')
+let input = prompt(`Guess a number between 1 & 10 ${userName}!`)
 let number = Number(input);
-let secretNum = 19
-var counts = 1
+var tries = 1
 var oldGuesses = []
 
 if (!isNaN(input)) {
 oldGuesses.push(input);
 }
 
-
-
-
-//Feature 1 and 2 - Guessing section - Higher or lower
 while (number !== secretNum) {
     if (Number.isNaN(number)) {
         alert('Not a valid number')
-        number = Number(prompt('Guess Again ' + nameInput + '!'));
+        number = Number(prompt('Guess Again ' + userName + '!'));
         oldGuesses.push(number)
 } else if (number < secretNum) {
-        number = Number(prompt('Guess Higher ' + nameInput + '!'));
-        counts++;
+        number = Number(prompt('Guess Higher ' + userName + '!'));
+        tries++;
         oldGuesses.push(number);
 } else if (number > secretNum) {
-        number = Number(prompt('Guess Lower ' + nameInput + '!'));
-        counts++;
+        number = Number(prompt('Guess Lower ' + userName + '!'));
+        tries++;
         oldGuesses.push(number);
     } 
 }
 
-if(number === secretNum) {
-    alert(`You got it ${nameInput}! Your number of tries was ${counts}! Your guesses were ${oldGuesses}!`)
-    } 
+//feature 7
+
+if (players[userName] !== undefined) {
+    let numGuesses = oldGuesses.length
+    let previousGuesses = players[userName].length
+        if(number === secretNum && numGuesses < previousGuesses) {
+            alert (`Congrats you got it! You beat your old score by ${ previousGuesses - numGuesses} guesses.`)
+        }else if(number === secretNum && numGuesses > previousGuesses) {
+            alert(`You guessed it! It took you ${numGuesses - previousGuesses} more tries than last time!`)
+        }else {
+            alert (`Congrats you guessed it! You got the same score!`)
+        }
+} else {
+    alert (`Congrats ${userName} you guessed it! Your number of tries was ${tries}. Your previous guesses were ${oldGuesses}.`)
+}
+players[userName];
+players[userName] = oldGuesses;
+
+
+playAgain = prompt('Do you want to play again?')
+if(playAgain.toLowerCase() == 'yes') {
+    playAgain = true;
+} else {
+    playAgain = false;
+}
+}
+
+
+
+
 
 
 
